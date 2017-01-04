ff42b9740cbc135653892923d98dc01219e212b0
Liu Bo <bo.li.liu@oracle.com>
2017-01-03 20:21:29 -0800

    Array: dynamic array with array re-alloc implementation
    Signed-off-by: Liu Bo <liub.liubo@gmail.com>

--- da.c
+++ da.c
@@ -9,86 +9,96 @@ int seq_n;  /* Sequence number */
 int q;  /* Query number */
 int lastans = 0;
 
-struct list {
-    struct list *prev;
-    struct list *next;
-    int v;
+struct array {
+	int n;	/* item count of the array */
+	int lastpos;	/* the last item position */
+	int *item; /* array starts here */
 };
 
-void list_add_tail(struct list *h, struct list *new)
+void append_seq(struct array *seq, int v)
 {
-    struct list *last = h->prev;
-    last->next = new;
-    new->prev = last;
-    new->next = h;
-    h->prev = new;
+	seq->item[seq->lastpos] = v;
+	seq->lastpos++;
+	/* array actual size is now seq->lastpos */
+
+	if (seq->lastpos == seq->n) {
+		/* enlarge this array by 2 */
+		char *new_item = malloc(sizeof(int) * 2 * seq->n);
+
+		/* copy the old to the new */
+		memcpy(new_item, (char *)seq->item, sizeof(int) * seq->n);
+		seq->item = (int *)new_item;
+		seq->n *= 2;
+		if (0) {
+			int i;
+			for (i = 0; i < seq->lastpos; i++)
+				printf("%d, ", seq->item[i]);
+			printf("\n");
+		}
+	}
 }
 
 void run_query(int i)
 {
-    int type;
-    int q_index;
-    int x, y;
-    
-    /* q_arr[i][0] */
-    type = *((int *)q_arr + i * 3);
-    
-    /* q_arr[i][1] */
-    x = *((int *)q_arr + i * 3 + 1);
-    
-    /* q_arr[i][2] */
-    y = *((int *)q_arr + i * 3 + 2);
+	int type;
+	int q_index;
+	int x, y;
 
-    q_index = (x ^ lastans) % seq_n;
-    if (type == 1) {
-        /* append y to the selected seq */
-        struct list *head = (struct list *)seq_arr + q_index;
-        struct list *insert;
-        insert = malloc(sizeof(struct list));
-        insert->v = y;
-        list_add_tail(head, insert);
-    } else if (type == 2) {
-        /* assign the found value to lastans */
-        struct list *head = (struct list *)seq_arr + q_index;
-	struct list *l = head->next;
-        int j;
-        
-        for (j = 0; j < y; j++) {
-            l = l->next;
-        }
-        lastans = l->v;
-        printf("%d\n", lastans);
-    }
+	/* q_arr[i][0] */
+	type = *((int *)q_arr + i * 3);
+	/* q_arr[i][1] */
+	x = *((int *)q_arr + i * 3 + 1);
+	/* q_arr[i][2] */
+	y = *((int *)q_arr + i * 3 + 2);
+
+	q_index = (x ^ lastans) % seq_n;
+	if (type == 1) {
+		/* append y to the selected seq */
+		struct array * seq = (struct array *)seq_arr + q_index;
+//		printf("%d %d\n", seq->n, seq->lastpos);
+		append_seq(seq, y);
+	} else if (type == 2) {
+		/* assign the found value at y % size to lastans */
+		struct array * seq = (struct array *)seq_arr + q_index;
+		int index = y % seq->lastpos;
+
+		lastans = seq->item[index];
+		printf("%d\n", lastans);
+	}
 }
 
 int main() {
-    int i;
-    
-    scanf("%d %d", &seq_n, &q);
-    /* each query consists of 3 items. */
-    q_arr = malloc(3 * sizeof(int) * q);
-    seq_arr = malloc(sizeof(struct list) * seq_n);
+	int i;
+	int init_cnt = 1;
 
-    for (i = 0; i < seq_n; i++) {
-        struct list *head;
-        head = (struct list *)seq_arr + i;
-        head->next = head;
-        head->prev = head;
-    }
+	scanf("%d %d", &seq_n, &q);
+	/* each query consists of 3 items. */
+	q_arr = malloc(3 * sizeof(int) * q);
+	seq_arr = malloc(sizeof(struct array) * seq_n);
 
-    /* input for each query */
-    for (i = 0; i < q; i++) {
-        scanf("%d %d %d", (int *)q_arr + (i * 3), (int *)q_arr + (i * 3) + 1, (int *)q_arr + (i * 3) + 2);
-    }
+	/* initial each array with 16 preallocated slots */
+	for (i = 0; i < seq_n; i++) {
+		struct array * seq = (struct array *)seq_arr + i;
+		char *seq_pointer = malloc(sizeof(int) * init_cnt);
 
-    /* execute each query */
-    for (i = 0; i < q; i++) {
-        run_query(i);
-    }
-    
-    free(seq_arr);
-    free(q_arr);
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
-    return 0;
+		seq->item = (int *)seq_pointer;
+		seq->n = init_cnt;
+		seq->lastpos = 0;
+	}
+
+	/* input for each query */
+	for (i = 0; i < q; i++) {
+		scanf("%d %d %d", (int *)q_arr + (i * 3), (int *)q_arr + (i * 3) + 1, (int *)q_arr + (i * 3) + 2);
+	}
+
+	/* execute each query */
+	for (i = 0; i < q; i++) {
+		run_query(i);
+	}
+
+	free(seq_arr);
+	free(q_arr);
+	/* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+	return 0;
 }
 
